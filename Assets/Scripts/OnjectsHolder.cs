d8d89aa430a37b25cef1d075cd1300f9ce2937bd
Petr Kotáb <petrkotab99@gmail.com>
2024-02-19 16:38:37 +0100

    feat(bugfix): fixed bug, where instances of buildings and resources were not loaded

---
+++ Assets/Scripts/OnjectsHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectHolderController : MonoBehaviour
+{
+    public List<ResourceScriptableObject> resources;
+    public List<BuildingScriptableObject> buildings;
+}
